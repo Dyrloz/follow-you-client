e00c526745007d859115fcf0ad3b3825a58d1736
Dyrloz <dyrloz@gmail.com>
2022-12-22 21:03:46 +0100

    feat: add generateTraceId - generateRegexTraceId - isTraceId

--- follow-you.js
+++ follow-you.js
@@ -1,3 +1,68 @@
-class FollowYou {}
+const ID_LENGTH = 20;
+const ID_NB_SEPARATION = 4;
+
+class FollowYou {
+    static ID_CHARACTERS =
+        'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    static ID_SEPARATION = '-';
+    static ID_INDEX_SEPARATION = Math.trunc(ID_LENGTH / ID_NB_SEPARATION);
+    static ID_REGEX = FollowYou.generateRegexTraceId();
+
+    /**
+     * Generate a random traceId based on variables.
+     * @returns String TraceId generated.
+     */
+    static generateTraceId() {
+        let traceId = '';
+
+        for (let i = 1; i <= ID_LENGTH + ID_NB_SEPARATION; i++) {
+            if (i % FollowYou.ID_INDEX_SEPARATION === 0) {
+                traceId += FollowYou.ID_SEPARATION;
+            } else {
+                traceId += FollowYou.ID_CHARACTERS.charAt(
+                    Math.floor(Math.random() * FollowYou.ID_CHARACTERS.length),
+                );
+            }
+        }
+
+        return traceId;
+    }
+
+    /**
+     * Generate a regex for the traceId based on variable.
+     * @returns RegExp Regex generated.
+     */
+    static generateRegexTraceId() {
+        let regexTraceId = '';
+        const rangeCharacters = '[' + FollowYou.ID_CHARACTERS + ']';
+
+        for (let i = 1; i <= ID_LENGTH + ID_NB_SEPARATION; i++) {
+            if (i % FollowYou.ID_INDEX_SEPARATION === 0) {
+                regexTraceId += FollowYou.ID_SEPARATION;
+            } else {
+                regexTraceId += rangeCharacters;
+            }
+        }
+
+        return new RegExp(regexTraceId);
+    }
+
+    /**
+     * Check if the traceId match the regex generated.
+     * @param {string} traceId
+     * @returns Boolean true if traceId match regex.
+     */
+    static isTraceId(traceId) {
+        if (!traceId) {
+            return false;
+        }
+
+        if (typeof traceId !== 'string') {
+            return false;
+        }
+
+        return traceId.match(FollowYou.ID_REGEX) !== null;
+    }
+}
 
 module.exports = { FollowYou };
--- follow-you.test.js
+++ follow-you.test.js
@@ -1,5 +1,80 @@
+const { FollowYou } = require('./follow-you');
+const ID_LENGTH = 20;
+const ID_NB_SEPARATION = 4;
+
 describe('Follow you', () => {
-    it('should be true', () => {
-        expect(true).toBe(true);
+    console.log(FollowYou.ID_INDEX_SEPARATION);
+
+    describe('generate trace id', () => {
+        const traceId = FollowYou.generateTraceId();
+
+        it('should match total length', () => {
+            expect(traceId.length).toEqual(ID_LENGTH + ID_NB_SEPARATION);
+        });
+
+        it('should have the same number of separation', () => {
+            expect(traceId.split(FollowYou.ID_SEPARATION).length).toEqual(
+                ID_NB_SEPARATION + 1,
+            );
+        });
+
+        it('should have the same number of random characters', () => {
+            expect(
+                traceId
+                    .split(FollowYou.ID_SEPARATION)
+                    .reduce(
+                        (total, characters) => total + characters.length,
+                        0,
+                    ),
+            ).toEqual(ID_LENGTH);
+        });
+    });
+
+    describe('generate regex trace id', () => {
+        const regexTraceId = FollowYou.generateRegexTraceId();
+        const stringRegexTraceId = regexTraceId.toString();
+
+        it('should be a regex', () => {
+            expect(regexTraceId instanceof RegExp).toBe(true);
+        });
+
+        it('should have the same number of separation', () => {
+            expect(
+                stringRegexTraceId.split(FollowYou.ID_SEPARATION).length,
+            ).toEqual(ID_NB_SEPARATION + 1);
+        });
+
+        it('should have the same number of random characters', () => {
+            expect(
+                [...stringRegexTraceId.matchAll(FollowYou.ID_SEPARATION)]
+                    .length,
+            ).toEqual(ID_NB_SEPARATION);
+        });
+    });
+
+    describe('Check if traceId is correct', () => {
+        it('should be true', () => {
+            expect(FollowYou.isTraceId(FollowYou.generateTraceId())).toBe(true);
+        });
+
+        it('with wrong pattern should be false', () => {
+            expect(FollowYou.isTraceId('hiitisabadpattern')).toBe(false);
+        });
+
+        it('with number should be false', () => {
+            expect(FollowYou.isTraceId(321)).toBe(false);
+        });
+
+        it('with empty string should be false', () => {
+            expect(FollowYou.isTraceId('')).toBe(false);
+        });
+
+        it('with null should be false', () => {
+            expect(FollowYou.isTraceId(null)).toBe(false);
+        });
+
+        it('with nothing should be false', () => {
+            expect(FollowYou.isTraceId()).toBe(false);
+        });
     });
 });
